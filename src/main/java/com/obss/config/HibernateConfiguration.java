b1b55e8bf5475cb7cda74f17e839dd9853079d1b
Omer Gurarslan <omer.gurarslan@obss.com.tr>
2013-05-10 14:47:14 +0300

    pom updated for local and heroku configs

--- src/main/java/com/obss/config/HibernateConfiguration.java
+++ src/main/java/com/obss/config/HibernateConfiguration.java
@@ -9,15 +9,27 @@ import org.hibernate.service.ServiceRegistryBuilder;
 public class HibernateConfiguration {
 	
 	public static ServiceRegistry hibernateConfig() {
-		Properties properties = new Properties();
-		properties.setProperty(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
-		properties.setProperty(Environment.URL, "jdbc:mysql://us-cdbr-east-03.cleardb.com/heroku_f89f3481d9cb626?reconnect=true");
-		properties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
-		properties.setProperty(Environment.USER, "b4d1de09400aed");
-		properties.setProperty(Environment.PASS, "a1135ae7");
+		Properties herokuProperties = new Properties();
+		herokuProperties.setProperty(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+		herokuProperties.setProperty(Environment.URL, "jdbc:mysql://us-cdbr-east-03.cleardb.com/heroku_f89f3481d9cb626?reconnect=true");
+		herokuProperties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
+		herokuProperties.setProperty(Environment.USER, "b4d1de09400aed");
+		herokuProperties.setProperty(Environment.PASS, "a1135ae7");
+		
+		Properties localProperties = new Properties();
+		localProperties.setProperty(Environment.DIALECT,"org.hibernate.dialect.MySQLDialect");
+		localProperties.setProperty(Environment.URL, "jdbc:mysql://localhost:3306/demoschema");
+		localProperties.setProperty(Environment.DRIVER, "com.mysql.jdbc.Driver");
+		localProperties.setProperty(Environment.USER, "root");
+		localProperties.setProperty(Environment.PASS, "");
 
 		ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
-		serviceRegistryBuilder.applySettings(properties);
+		
+		//heroku
+		//serviceRegistryBuilder.applySettings(herokuProperties);
+		
+		//local
+		serviceRegistryBuilder.applySettings(localProperties);
 		
 		ServiceRegistry serviceRegistry = serviceRegistryBuilder.buildServiceRegistry();
 		return serviceRegistry;
